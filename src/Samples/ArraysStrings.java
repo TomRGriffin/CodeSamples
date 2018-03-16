29519d801ffe8c9e9421e50950bb40bf9a1eed50
Rahul <rahul.nimbalkar@nokia.com>
2018-03-15 18:56:44 -0700

    Merge sorted arrays

--- src/Samples/ArraysStrings.java
+++ src/Samples/ArraysStrings.java
@@ -1,4 +1,5 @@
 package Samples;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -428,6 +429,36 @@ public class ArraysStrings {
 		}
 
 		return nums.length + 1;
+	}
+
+//	Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+//			Note:
+//	You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements
+// from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+
+	public static void testMergeArray () {
+		int [] nums1 = new int[] { 1, 3, 5, 0, 0, 0};
+		int m = 3;
+		int [] nums2 = new int[] { 2, 4, 6};
+		int n = 3;
+		new ArraysStrings().merge(nums1, m, nums2, n);
+	}
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+	    int end = m  + n - 1;
+		int i = m - 1;
+		int j = n - 1;
+
+		while (i >= 0 && j >= 0) {
+			if (nums1[i] >= nums2[j]) {
+				nums1[end--] = nums1[i];
+				i--;
+			} else {
+				nums1[end--] = nums2[j];
+				j--;
+			}
+		}
+		System.out.println("Merged Array = " + Arrays.toString(nums1));
 	}
 }
 
